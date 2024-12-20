bc6a33efa910f059469cae4d46108a8307758f0d
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2024-12-20 18:24:36 +0530

    Feat [backend]: enhance authentication logic to support username and improve validation and error handling

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,50 +1,54 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { UserService } from "../services/userService";
-import { ZodError } from "zod";
-
+import { publicUserSchema } from "../schemas";
 class AuthController {
-  async login(req: Request, res: Response) {
-    const { email, password } = req.body;
+  async login(req: Request, res: Response, next: NextFunction) {
+    const { email, password, username } = req.body;
     try {
-      const user = await UserService.findUserByEmail(email);
+      const user = await UserService.findByUsernameOrEmail({ email, username });
 
       if (!user) {
-        res.status(404).json({ message: "No user found with this email" });
+        res.status(404).json({
+          message: "No user found with this" + email ? "email" : "username",
+        });
         return;
       }
 
-      if (UserService.checkPassword.bind(user, password)) {
+      if (!(await UserService.checkPassword(user, password))) {
         res
           .status(401)
           .json({ message: "Invalid password or email combination." });
         return;
       }
+
+      res.status(200).json({
+        success: true,
+        message: "User logged in successfully",
+        data: publicUserSchema.parse(user),
+      });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Internal Server Error" });
+      next(error);
     }
   }
 
-  async register(req: Request, res: Response) {
+  async register(req: Request, res: Response, next: NextFunction) {
     try {
       const { name, username, email, password } = req.body;
-      console.log(req.body);
+      await UserService.validateUser({ name, username, email, password });
       const user = await UserService.createUser({
         name,
         username,
         email,
         password,
       });
-      res.status(201).json(user);
+      res.status(201).json({
+        success: true,
+        message: "User created successfully",
+        data: publicUserSchema.parse(user),
+      });
       return;
-    } catch (error) {
-      if (error instanceof ZodError) {
-        // console.error(error.errors);
-        res.status(500).json({ message: error.errors });
-        return;
-      }
-      // console.error(error);
-      res.status(500).json({ message: "Internal Server Error" });
+    } catch (error: any) {
+      next(error);
     }
   }
 }
--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -1,8 +1,39 @@
-import { users, IUser, userInsertSchema } from "../db/schema";
+import { users } from "../db/schema";
+import { DuplicateUserError } from "../errors/duplicateUserError";
+import { userInsertSchema } from "../schemas";
+import { IUser } from "../types/user";
 import { compareHashedPassword, hashPassword } from "../utils/passwordHash";
-import { db } from "./db";
+import { db } from "../db/db";
+
+declare interface usernameOrEmail {
+  username?: string;
+  email?: string;
+}
 
 export class UserService {
+  static async validateUser({
+    name,
+    password,
+    username,
+    email,
+  }: {
+    name: string;
+    email: string;
+    password: string;
+    username: string;
+  }) {
+    const valid = userInsertSchema.safeParse({
+      name,
+      email,
+      password,
+      username,
+    });
+
+    if (!valid.success) {
+      return Promise.reject(valid.error);
+    }
+  }
+
   static async createUser({
     name,
     email,
@@ -15,11 +46,30 @@ export class UserService {
     username: string;
   }) {
     try {
-      userInsertSchema.parse({ name, email, password, username });
       const hashedPassword = await hashPassword(password);
+      const duplicateEmail = await this.findUserByEmail(email);
+      if (duplicateEmail) {
+        return Promise.reject(
+          new DuplicateUserError("User with this email already exists")
+        );
+      }
+      const duplicateUsername = await this.findByUsernameOrEmail({
+        username,
+      });
+      if (duplicateUsername) {
+        return Promise.reject(
+          new DuplicateUserError("Username is already taken")
+        );
+      }
       const user = await db
         .insert(users)
-        .values({ name, email, password: hashedPassword, username });
+        .values({ name, email, password: hashedPassword, username })
+        .returning({
+          id: users.id,
+          name: users.name,
+          email: users.email,
+          username: users.username,
+        });
       return user;
     } catch (error) {
       return Promise.reject(error);
@@ -30,6 +80,21 @@ export class UserService {
     try {
       const user = await db.query.users.findFirst({
         where: (user, { eq }) => eq(user.email, email),
+      });
+      return user;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  static async findByUsernameOrEmail({
+    email = "",
+    username = "",
+  }: usernameOrEmail) {
+    try {
+      const user = await db.query.users.findFirst({
+        where: (user, { or, eq }) =>
+          or(eq(user.email, email), eq(user.username, username)),
       });
       return user;
     } catch (error) {
@@ -48,7 +113,7 @@ export class UserService {
     }
   }
 
-  static async checkPassword(this: IUser, password: string) {
-    return compareHashedPassword(password, this.password);
+  static async checkPassword(user: IUser, password: string) {
+    return await compareHashedPassword(password, user.password);
   }
 }
