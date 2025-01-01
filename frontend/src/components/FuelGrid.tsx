562b3c0ae32d74cb9f91b788d0fe3da011f87b98
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-01-01 23:45:44 +0530

    Feat [frontend]: implement infinite scroll in FuelGrid component and add loading indicator

--- frontend/src/components/FuelGrid.tsx
+++ frontend/src/components/FuelGrid.tsx
@@ -1,4 +1,7 @@
+import useInfiniteScroll from "react-infinite-scroll-hook";
 import FuelCard from "./FuelCard";
+import { useState } from "react";
+import { LoaderIcon } from "lucide-react";
 
 const fuels = [
   {
@@ -17,14 +20,71 @@ const fuels = [
     id: "sddfgdsfj",
     type: "gallery",
   },
+  {
+    id: "sdjfhsfj",
+    type: "gallery",
+  },
+  {
+    id: "fghfhf",
+    type: "text",
+  },
+  {
+    id: "fghfghfgh",
+    type: "create",
+  },
+  {
+    id: "sddfgdsfj",
+    type: "gallery",
+  },
 ];
 
+// paginate fuels , each time 2 items
+function useLoadFuels() {
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [error] = useState(null);
+  const [items, setItems] = useState(fuels.slice(0, 2));
+
+  const loadMore = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setItems([...items, ...fuels.slice(page * 2, page * 2 + 2)]);
+      setPage(page + 1);
+      setLoading(false);
+      if (page >= fuels.length / 2) {
+        setHasNextPage(false);
+      }
+    }, 2000);
+  };
+  return { loading, items, hasNextPage, error, loadMore };
+}
+
 function FuelGrid() {
+  const { loading, items, hasNextPage, error, loadMore } = useLoadFuels();
+
+  const [sentryRef, { rootRef }] = useInfiniteScroll({
+    loading,
+    hasNextPage,
+    onLoadMore: loadMore,
+    disabled: !!error,
+    rootMargin: "0px 0px 400px 0px",
+  });
+
   return (
-    <div className="grid grid-cols-1 gap-3">
-      {fuels.map((fuel) => {
-        return <FuelCard type={fuel.type} key={fuel.id}></FuelCard>;
+    <div ref={rootRef} className="grid grid-cols-1 gap-3">
+      {items.map((fuel) => {
+        return (
+          <FuelCard ref={sentryRef} type={fuel.type} key={fuel.id}></FuelCard>
+        );
       })}
+      {(loading || hasNextPage) && (
+        <div className="flex items-center justify-center py-5">
+          <div className="animate-spin">
+            <LoaderIcon className="size-8" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
